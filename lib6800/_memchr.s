a4b402553e09cd3d96a0e045f5256f7f1d436d67
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-03 18:12:19 +0900

    add lib6800/_memchr.s

---
+++ lib6800/_memchr.s
@@ -0,0 +1,36 @@
+;
+;	void *memchr(const void *s,int c,size_t n)
+;
+	.export _memchr
+	.setcpu 6800
+	.code
+
+_memchr:
+	tsx
+	ldx 2,x
+	beq retnull		; n == 0, return NULL
+	tsx
+	ldab 3,x
+	ldaa 2,x
+	addb 7,x
+	adca 6,x
+	stab @tmp+1
+	staa @tmp		; stop point
+;
+	ldab 5,x
+	ldx 2,x			; start point
+loop:
+	cmpb ,x
+	beq match
+	inx
+	cpx @tmp
+	bne loop
+retnull:
+	clra
+	clrb
+	rts
+match:
+	stx @tmp
+	ldab @tmp+1
+	ldaa @tmp
+	rts
